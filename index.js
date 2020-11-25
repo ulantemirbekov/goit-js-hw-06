caa13d1d85672f2fdde1d56117fd5ef5f315ac55
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-25 15:10:26 +0200

    task-03 solution added

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ const getUsersWithEyeColor = (users, color) => {
 
 console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
+
+// Задание 3
+const getUsersWithGender = (users, gender) => {
+    return users.filter(user => user.gender === gender).map(user => user.name)
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
