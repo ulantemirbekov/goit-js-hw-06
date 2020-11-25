e601e3a61eb038f54ddc74a59e1b9e58091726b4
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-25 15:54:57 +0200

    task-06 solution added

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект 
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
 
+// Задание 6
+const getUsersWithAge = (users, min, max) => {
+    return users.filter(user => user.age >= min && user.age <= max)
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 
 
