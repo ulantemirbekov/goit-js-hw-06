0bd184b4f0ff90d385e0c8d4dd7104285159b46f
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-25 15:04:03 +0200

    task-02 solution added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import users from "./users.js";
 
+// Задание 1
 const getUserNames = users => {
     return users.map(user => user.name);
 };
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+// Задание 2
+const getUsersWithEyeColor = (users, color) => {
+    return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
